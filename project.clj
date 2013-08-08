8c21d9de62b4edb1938530bc1616cf43d5eb271c
Tim Gilbert <timothy_l.gilbert@nytimes.com>
2013-08-08 18:14:37 -0400

    apleph chatroom

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.5"]]
-  :ring {:handler sesquipedalian.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+                 [compojure "1.1.5"]
+                 [aleph "0.3.0-rc2"]
+                 [lamina "0.5.0-rc4"]]
+  ;:plugins [[lein-ring "0.8.5"]]
+  ;:ring {:handler sesquipedalian.handler/app}
+  ;:profiles
+  ;{:dev {:dependencies [[ring-mock "0.1.5"]]}}
+  :main sesquipedalian.core)
