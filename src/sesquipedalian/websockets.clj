dba6b2c6789b69f892c1edd695f84b1006f903ab
Tim Gilbert <timgilbert@gmail.com>
2013-08-18 17:21:21 -0400

    All tests work now

--- src/sesquipedalian/websockets.clj
+++ src/sesquipedalian/websockets.clj
@@ -4,7 +4,7 @@
 (defn- make-name-pool [anons named] [anons named])
 
 (defn find-key-for [map value]
-  (for [[k v] map :when (= v value)] k))
+  (first (for [[k v] map :when (= v value)] k)))
 
 (defn new-name-pool []
   "Returns a new empty client list"
@@ -15,14 +15,16 @@
 (defn add-anonymous-member [[anon named] new-item]
   [(conj anon new-item) named])
 
-(defn name-member [[anon named] item name]
+(defn name-member [[anon named] name item]
   [(disj anon item)
    (assoc named name item )])
 
 (defn remove-member [[anon named] former-item]
-  (debug "removing" former-item)
-  [(disj anon former-item)
-   (dissoc named (find-key-for named former-item))])
+  (let [new-anon  (disj anon former-item)
+        key       (find-key-for named former-item)
+        new-named (dissoc named key)]
+  (debug "db:" new-anon key new-named)
+  [new-anon new-named]))
 
 (defn get-named-member [[anon named] name]
   (named name))
